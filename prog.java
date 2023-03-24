7fd9c1b626b86db0c6663400a9da89c3bb21b4de
Artemiev Dima <adbart1985@gmail.com>
2023-03-24 08:07:37 +0300

    first commit

---
+++ prog.java
@@ -0,0 +1,14 @@
+/**
+ * prog
+ */
+public class prog {
+    public static void main(String[] args) {
+        String s = " 4";
+
+        System.out.println(s);
+
+        
+        System.out.println("Hello world!"); // коменатрий   
+    }
+}
+
